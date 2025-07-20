ac0a5092fe51379f70978b1520462818b25fdda2
IamLRBA <jerrylarubafestus@gmail.com>
2025-07-20 09:27:41 +0300

    UI/UX Mods

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,12 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Products from './pages/Products/Products';
-import ShirtsAndTees from './pages/Products/ShirtsAndTees';
-import Bottoms from './pages/Products/Bottoms';
-import CoatsAndJackets from './pages/Products/CoatsAndJackets';
-import Footwear from './pages/Products/Footwear';
-import Accessories from './pages/Products/Accessories';
-import Underwear from './pages/Products/Underwear';
 import Gallery from './pages/Gallery/Gallery';
 import Contact from './pages/Contact/Contact';
 import Order from './pages/Order/Order';
@@ -30,13 +24,6 @@ const AppRoutes = () => {
       
       {/* Products Routes */}
       <Route path="/products" element={<Products />} />
-      <Route path="/products/shirts-and-tees" element={<ShirtsAndTees />} />
-      <Route path="/products/bottoms" element={<Bottoms />} />
-      <Route path="/products/coats-and-jackets" element={<CoatsAndJackets />} />
-      <Route path="/products/footwear" element={<Footwear />} />
-      <Route path="/products/accessories" element={<Accessories />} />
-      <Route path="/products/underwear" element={<Underwear />} />
-      <Route path="/products/order" element={<Order />} />
       
       {/*   Gallery Routes */}
       <Route path="/gallery" element={<Gallery />} />
